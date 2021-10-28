023c6262ef36a2db9ced875b634a4ccc80aad9da
ypyvovar <yurii_pyvovarenko_tp@bmc.com>
2021-10-28 22:46:21 +0300

    Created first class with the "multiply(a,b)" method

---
+++ src/org/corejava/Main.java
@@ -0,0 +1,13 @@
+package org.corejava;
+
+/**
+ * Created by yurii.pyvovarenko on 10/28/2021.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("multiply(2,3) = "+multiply(2,3));
+    }
+    static long multiply(long a, long b) {
+        return a*b;
+    }
+}
