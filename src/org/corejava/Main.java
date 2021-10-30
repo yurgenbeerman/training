c070034dd5f3366a49d8c40095a479cd915edea3
ypyvovar <yurii_pyvovarenko_tp@bmc.com>
2021-10-30 14:50:15 +0300

    cleaned code from git labels

--- src/org/corejava/Main.java
+++ src/org/corejava/Main.java
@@ -5,11 +5,9 @@ package org.corejava;
  */
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("decrement(2,3) = "+decrement(2,3));
     }
     static long decrement(long a, long b) {
         return a-b;
->>>>>>> 86d8376 (replaced "multiply" method with "sum")
     }
 }
