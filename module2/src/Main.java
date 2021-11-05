4afcd8ba807c5d6922793ae48c2d4696ded4c7ea
ypyvovar <yurii_pyvovarenko_tp@bmc.com>
2021-11-05 22:36:07 +0200

    implemented "joinTwoSortedArrays" method

--- module2/src/Main.java
+++ module2/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Main {
@@ -10,6 +11,7 @@ public class Main {
         System.out.println("Module2");
 
         //1. Объединить два отсортированных массива. "нужно считать что оба отсортированы, и решить за n"
+        joinTwoSortedArrays();
 
         //2. Реверс строки с помощью стека
 
@@ -27,8 +29,45 @@ public class Main {
     }
 
     private static void joinTwoSortedArrays() {
-        int[] aray1 = {1,5,8,24,98,99,123};
-        int[] aray2 = {9,56,132,212};
+        System.out.println("joinTwoSortedArrays");
+        int[] array1 = {}; //{1,5,8,24,98,99,123};
+        final int length1 = array1.length;
+        int posOfMin1 = 0;
+        int lastMin1;
+
+        int[] array2 = {}; //{9,56,132,212};
+        final int length2 = array2.length;
+        int posOfMin2 = 0;
+        int lastMin2;
+
+        int[] resultArray = new int[length1+length2];
+
+        for(int i=0; i<length1+length2;i++) {
+            if(posOfMin1>=length1 && posOfMin2<length2) {
+                resultArray[i] = array2[posOfMin2];
+                posOfMin2++;
+            } else if(posOfMin2>=length2 && posOfMin1<length1) {
+                resultArray[i] = array1[posOfMin1];
+                posOfMin1++;
+            } else if(posOfMin1<length1 && posOfMin2<length2) {
+                lastMin1 = array1[posOfMin1];
+                lastMin2 = array2[posOfMin2];
+                if(lastMin1 > lastMin2) {
+                    resultArray[i] = array2[posOfMin2];
+                    posOfMin2++;
+                } else if(lastMin1 < lastMin2) {
+                    resultArray[i] = array1[posOfMin1];
+                    posOfMin1++;
+                } else {
+                    resultArray[i] = array1[posOfMin1];
+                    i++;
+                    resultArray[i] = array2[posOfMin2];
+                    posOfMin1++;
+                    posOfMin1++;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(resultArray));
     }
 
     private static void revertStringUsingStack(String string) {
