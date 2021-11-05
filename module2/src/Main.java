8a0cfe9d0440c2411eb3605f0c82996be69d2eca
ypyvovar <yurii_pyvovarenko_tp@bmc.com>
2021-11-05 18:02:54 +0200

    clarified first task. Started implementation.

--- module2/src/Main.java
+++ module2/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class Main {
 
     /**
@@ -7,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Module2");
 
-        //1. Объединить два отсортированных массива
+        //1. Объединить два отсортированных массива. "нужно считать что оба отсортированы, и решить за n"
 
         //2. Реверс строки с помощью стека
 
@@ -22,5 +24,14 @@ public class Main {
         //7. Вывести все слова Trie ( префиксное деревое )
 
         //8. Найти симметричные пары в массиве
+    }
+
+    private static void joinTwoSortedArrays() {
+        int[] aray1 = {1,5,8,24,98,99,123};
+        int[] aray2 = {9,56,132,212};
+    }
+
+    private static void revertStringUsingStack(String string) {
+        //Stack
     }
 }
